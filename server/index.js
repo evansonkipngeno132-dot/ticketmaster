3076ff69ac334b115146445e04a5ba321078cbee
unknown <admin@example.com>
2026-06-18 22:08:58 +0300

    Fix dotenv loading path to use server directory .env file

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 const crypto = require('crypto');
 
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
